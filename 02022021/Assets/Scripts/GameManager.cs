941a73dcd140c513f9dffe9487cd09ef4b269a4d
MoritzFNelly <78220890+MoritzFNelly@users.noreply.github.com>
2021-02-16 10:15:02 +0100

    Step 1
    Straßen setzten und verbinden

---
+++ 02022021/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using SVS;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public RoadManager roadmanager;
+    public CameraMovement cameraMovement;
+    public InputManager inputManager;
+
+    private void Start()
+    {
+        inputManager.OnMouseClick += HandleMouseClick;
+    }
+
+    private void HandleMouseClick(Vector3Int position)
+    {
+        Debug.Log(position);
+        roadmanager.PlaceRoad(position);
+    }
+
+    private void Update()
+    {
+        cameraMovement.MoveCamera(new Vector3(inputManager.CameraMovementVector.x, 0, inputManager.CameraMovementVector.y));
+    }
+}
---
+++ 02022021/Assets/Scripts/RoadManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadManager : MonoBehaviour
+{
+    public PlacementManager placementManager;
+    public RoadFixer roadFixer;
+
+    public List<Vector3Int> temporaryPlacementPositions = new List<Vector3Int>();
+    public List<Vector3Int> roadPositionsToRecheck = new List<Vector3Int>();
+
+    private void Start()
+    {
+        roadFixer = GetComponent<RoadFixer>();
+    }
+
+    public void PlaceRoad(Vector3Int position)
+    {
+        //Überprüfung der ausgewählten Position
+        if (!placementManager.CheckIfPositionInBound(position)) return;
+        if (!placementManager.CheckIfPositionIsFree(position)) return;
+
+        temporaryPlacementPositions.Clear();
+        roadPositionsToRecheck.Clear();
+        temporaryPlacementPositions.Add(position);
+        placementManager.PlaceTemporaryStructure(position, roadFixer.deadEnd, CellType.Road); // Base-Prefab
+        FixRoadPrefabs();
+    }
+
+    private void FixRoadPrefabs()
+    {
+        foreach(var temporaryPosition in temporaryPlacementPositions)
+        {
+            roadFixer.FixRoadAtPosition(placementManager, temporaryPosition);
+            var neighbours = placementManager.GetNeighboursOfTypeFor(temporaryPosition, CellType.Road);
+            foreach (var roadposition in neighbours)
+            {
+                roadPositionsToRecheck.Add(roadposition);
+            }
+        }
+
+        foreach (var positionToFix in roadPositionsToRecheck)
+        {
+            roadFixer.FixRoadAtPosition(placementManager, positionToFix);
+        }
+    }
+}
